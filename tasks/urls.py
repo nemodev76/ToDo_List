3ccd7137b751b50fc21fbb62a4012a71a9ca8f18
nemodev76 <anmar.wit@gmail.com>
2025-01-27 23:44:43 +0300

    Refactor task management: add check/uncheck functionality, edit task feature, and update URL patterns

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_task, name='add_task'),
-    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('update/<int:task_id>/', views.update_task, name='update_task'),
+    path('check/<int:id>/', views.check_task, name='check_task'),
+    path('uncheck/<int:id>/', views.uncheck_task, name='uncheck_task'),
+    path('delete/<int:id>/', views.delete_task, name='delete_task'),
+    path('edit/<int:id>/', views.edit_task, name='edit_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,21 +3,38 @@ from .models import Task
 
 # Create your views here.
 def add_task(request):
-    task = request.POST['task']
-    Task.objects.create(title=task)
+    title = request.POST['task_title']
+    Task.objects.create(title=title)
     return redirect('home')
 
-def delete_task(request, task_id):
-    task = Task.objects.get(id=task_id)
-    task.delete()
-    return render(request, 'home.html')
-
-def update_task(request, task_id):
-    task = Task.objects.get(id=task_id)
-    task.is_completed = not task.is_completed
+def check_task(request, id):
+    task = Task.objects.get(id=id)
+    task.is_completed = True
     task.save()
-    return render(request, 'home.html')
+    return redirect('home')
 
-# def task_detail(request, task_id):
-#     task = Task.objects.get(id=task_id)
+def uncheck_task(request, id):
+    task = Task.objects.get(id=id)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def delete_task(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('home')
+
+def edit_task(request, id):
+    task = Task.objects.get(id=id)
+    if request.method == 'POST':
+        new_title = request.POST.get('new_task_title')
+        task.title = new_title
+        task.save()
+        return redirect('home')
+        # return JsonResponse({'success': True, 'new_task': new_task})
+    # return JsonResponse({'success': False, 'error': 'Invalid request'})
+    return render(request, 'tasks/edit_task.html', {'task': task})
+
+# def task_detail(request, id):
+#     task = Task.objects.get(id=id)
 #     return render(request, 'tasks/task_detail.html', {'task': task})
